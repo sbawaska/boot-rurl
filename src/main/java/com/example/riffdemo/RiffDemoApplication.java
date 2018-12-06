322103b287fd19457e0df6945aa07ac11225e6a8
Swapnil Bawaskar <sbawaskar@pivotal.io>
2018-12-06 14:40:10 -0800

    adding source

---
+++ src/main/java/com/example/riffdemo/RiffDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.riffdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiffDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiffDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/riffdemo/RiffDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.riffdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RiffDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
