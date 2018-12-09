12c275e620fe3245b99fa4e967509d3e220baf0b
Swapnil Bawaskar <sbawaskar@pivotal.io>
2018-12-08 16:35:49 -0800

    writing to channel

--- src/main/java/com/example/riffdemo/RiffDemoApplication.java
+++ src/main/java/com/example/riffdemo/RiffDemoApplication.java
@@ -1,20 +1,38 @@
 package com.example.riffdemo;
 
 import org.apache.commons.codec.digest.DigestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.function.Function;
 
 @SpringBootApplication
 public class RiffDemoApplication {
 
-    public static final int HASH_LENGTH = 8;
+    private static final int HASH_LENGTH = 8;
+    private static final String DOMAIN_NAME = "http://r.url/";
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Bean
     public Function<String, String> shortUrl() {
-        return s -> DigestUtils.md5Hex(s).substring(0, HASH_LENGTH);
+        return s -> {
+            String hash = DigestUtils.md5Hex(s).substring(0, HASH_LENGTH);
+            writeToChannel(hash, s);
+            return DOMAIN_NAME + hash;
+        };
+    }
+
+    private void writeToChannel(String hash, String longUrl) {
+        // TODO parameterize this
+        HttpEntity<String> kvPair = new HttpEntity<>(hash+":"+longUrl);
+        restTemplate.postForLocation("http://url-channel.default.svc.cluster.local", kvPair);
     }
 
 	public static void main(String[] args) {
